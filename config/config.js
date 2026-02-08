87f2d531cd9566dc4ce56c6ef5024b638abb6262
Husain Alnahash <hussainsaleh180@gmail.com>
2026-02-08 20:41:26 +0300

    config/config.js done

---
+++ config/config.js
@@ -0,0 +1,12 @@
+// ./config/config.js
+
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+
+mongoose.connection.once('open',()=>{
+    console.log('Mongo is showing us love')
+})
